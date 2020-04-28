2bd7d9fb1632252e2042d1133faf4d7f7f71865f
Henrique Cocito <henriquecocito@gmail.com>
2020-04-28 12:29:34 -0300

    Crate initial functions to merge a PR

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const automerge = require("./lib/automerge.js")
+const appId = require("./lib/id.js")
+
 /**
  * This is the main entrypoint to your Probot app
  * @param {import('probot').Application} app
@@ -6,14 +9,20 @@ module.exports = app => {
   // Your code here
   app.log('Yay, the app was loaded!')
 
-  app.on('issues.opened', async context => {
-    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
-    return context.github.issues.createComment(issueComment)
-  })
+  app.on([
+    'pull_request.opened', 
+    'pull_request.reopened', 
+    'pull_request.labeled', 
+    'pull_request.edited', 
+    'pull_request.ready_for_review'], async context => await automerge.run(context) )
 
-  // For more information on building apps:
-  // https://probot.github.io/docs/
-
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
+  app.on([
+    'check_run.created',
+    'check_run.completed'], async context => { 
+      if(context.payload.check_run.check_suite.app.id === appId()) {
+        return
+      }
+      
+      await automerge.run(context) 
+    })
 }
